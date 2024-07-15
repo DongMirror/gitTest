d9279ae349b7b477fe9bd23eab78a1af5e610667
DongminLee <ldm030475@naver.com>
2024-07-15 13:34:53 +0900

    git ignore

--- src/exam/test/Main.java
+++ src/exam/test/Main.java
@@ -3,5 +3,7 @@ package exam.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test tsteteteasdfasdf");
+        System.out.println("test tsteteteasdfasdf");
+        System.out.println("test tsteteteasdfasdf");
     }
 }
